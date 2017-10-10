b7d38947914888c568e12d638b57fbe02a12abd3
Leandro Rosa <leandro.rosa@neogrid.com>
2017-10-10 08:18:59 -0300

    Alterado para método POST

--- src/main/java/com/lrosa/robot/controller/RobotController.java
+++ src/main/java/com/lrosa/robot/controller/RobotController.java
@@ -7,8 +7,8 @@ import com.lrosa.robot.entity.Robot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,17 +16,16 @@ public class RobotController {
 
     private static final Logger LOG = LoggerFactory.getLogger(RobotController.class);
 
-
     private final Robot robot = new Robot();
 
-    @GetMapping(value = "/rest/mars/{command}")
+    @PostMapping(value = "/rest/mars/{command}")
     public ResponseEntity<String> move(@PathVariable("command") final String command) {
         try {
             robot.move(command);
             return ResponseEntity.accepted().body(robot.toString());
         } catch (CommandNotFoundException|InvalidRobotPositionException|InvalidCommandException e) {
             LOG.error(e.getMessage(), e);
-            return ResponseEntity.badRequest().body("400 - Bad request");
+            return ResponseEntity.badRequest().body("400 Bad request");
         }
     }
 }
