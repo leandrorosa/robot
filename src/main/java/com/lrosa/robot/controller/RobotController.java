49c91288d89e445719754423a17d9185534ec651
Leandro Rosa <leandro.rosa@neogrid.com>
2017-10-10 08:12:46 -0300

    alterações no pacote do projeto

--- src/main/java/com/lrosa/robot/controller/RobotController.java
+++ src/main/java/com/lrosa/robot/controller/RobotController.java
@@ -4,6 +4,8 @@ import com.lrosa.robot.command.exception.CommandNotFoundException;
 import com.lrosa.robot.command.exception.InvalidCommandException;
 import com.lrosa.robot.command.exception.InvalidRobotPositionException;
 import com.lrosa.robot.entity.Robot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RobotController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RobotController.class);
+
 
     private final Robot robot = new Robot();
 
@@ -20,7 +25,8 @@ public class RobotController {
             robot.move(command);
             return ResponseEntity.accepted().body(robot.toString());
         } catch (CommandNotFoundException|InvalidRobotPositionException|InvalidCommandException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            LOG.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().body("400 - Bad request");
         }
     }
 }
